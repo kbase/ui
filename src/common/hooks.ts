6f4ab55f76ca709d1995012cf206c8acc258c986
Dakota Blair <dblair@bnl.gov>
2023-01-13 12:14:46 -0500

    Create the useFilteredParams hook.
    This commit adds a hook which filters the URL search parameters and loads the
    value of known parameters into the state.

--- src/common/hooks.ts
+++ src/common/hooks.ts
@@ -1,6 +1,54 @@
+import { useEffect } from 'react';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router-dom';
 import type { RootState, AppDispatch } from '../app/store';
+import { isValidParam, setParams } from '../features/params/paramsSlice';
 
 // Use throughout the app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const ignoredParameterWarning = (ignored: string[]) =>
+  `Ignored parameters: ${ignored.join(', ')}`;
+
+// Filter URL Search Parameters to only those recognized in paramsSlice
+export const useFilteredParams = () => {
+  const dispatch = useAppDispatch();
+  const loc = useLocation();
+  const navigate = useNavigate();
+  const locSearch = new URLSearchParams(loc.search);
+  const paramsIgnored = Array.from(locSearch.entries())
+    .filter(([param]) => !isValidParam(param))
+    .map(([param]) => {
+      return param;
+    });
+  if (paramsIgnored.length) {
+    // eslint-disable-next-line no-console
+    console.log(ignoredParameterWarning(paramsIgnored));
+  }
+  const paramsFiltered = Object.fromEntries(
+    Array.from(locSearch.entries())
+      .filter(([param]) => isValidParam(param))
+      .map(([param, value]) => {
+        return [param, value];
+      })
+  );
+  const paramsQuery = new URLSearchParams(paramsFiltered).toString();
+  const check = Array.from(locSearch.entries())
+    .map(([param, value]) => paramsFiltered[param] === value)
+    .filter((entry) => !entry);
+  useEffect(() => {
+    if (check.length) {
+      navigate(`${loc.pathname}?${paramsQuery}`);
+    }
+    dispatch(setParams(paramsFiltered));
+  }, [
+    check.length,
+    dispatch,
+    loc.pathname,
+    navigate,
+    paramsFiltered,
+    paramsQuery,
+  ]);
+  return paramsFiltered;
+};
