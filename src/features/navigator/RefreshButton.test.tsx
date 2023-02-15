a4da6f5272cd80e7d5cfdbcdc3e73890643dfa38
Dakota Blair <dblair@bnl.gov>
2023-02-15 16:38:15 -0500

    URO-143: Added searchapi2 using RTK Query.
    This commit adds support for making requests to searchapi2 and caching their
    results using RTK Query. The only supported request at present is searching for
    a list of narratives as a part of Navigator. The <RefreshButton /> component
    has been updated to invalidate these searches from the cache.
    
    An important update is that the `id` field in JSONRPC requests has been
    converted to a string type, which is mutually supported by JSONRPC 1.1 and
    JSONRPC 2.0.

--- src/features/navigator/RefreshButton.test.tsx
+++ src/features/navigator/RefreshButton.test.tsx
@@ -1,15 +1,8 @@
 import { fireEvent, render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { createTestStore } from '../../app/store';
-import { Category } from './common';
-import RefreshButton from './RefreshButton';
-
-const initialFreshState = {
-  category: Category['own'],
-  fresh: true,
-  narratives: [],
-  selected: null,
-};
+import type { AppDispatch } from '../../app/store';
+import RefreshButton, { refreshHandlerFactory } from './RefreshButton';
 
 test('RefreshButton renders', () => {
   const { container } = render(
@@ -21,14 +14,19 @@ test('RefreshButton renders', () => {
   expect(container.querySelector('.button.refresh')).toBeInTheDocument();
 });
 
-test('RefreshButton sets fresh to false when clicked', () => {
-  const store = createTestStore({ navigator: initialFreshState });
+test('RefreshButton executes handler when clicked.', () => {
+  const store = createTestStore();
+  const spy = jest.fn();
+  const clickHandlerFactory = (dispatch: AppDispatch) => () => {
+    spy();
+    refreshHandlerFactory(dispatch)();
+  };
   const { container } = render(
     <Provider store={store}>
-      <RefreshButton />
+      <RefreshButton clickHandlerFactory={clickHandlerFactory} />
     </Provider>
   );
   const refreshButton = container.querySelector('.button.refresh');
   refreshButton && fireEvent.click(refreshButton);
-  expect(store.getState().navigator.fresh).toBeFalsy();
+  expect(spy).toHaveBeenCalledTimes(1);
 });
