54f91601796c3f40b4984c775f557be531b2b5ea
David Lyon <dauglyon@users.noreply.github.com>
2023-03-15 15:17:33 -0700

    Add param slice management hooks

---
+++ src/features/params/hooks.ts
@@ -0,0 +1,32 @@
+import { useCallback } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useAppSelector } from '../../common/hooks';
+import { ParamsState, generatePathWithSearchParams } from './paramsSlice';
+
+export const useUpdateAppParams = () => {
+  const params = useAppSelector((state) => state.params);
+  const loc = useLocation();
+  const navigate = useNavigate();
+  return useCallback(
+    (updates: Partial<ParamsState>) => {
+      const newParams = { ...params };
+      Object.entries(updates).forEach(([key, value]) => {
+        if (value === undefined || value === null) {
+          delete newParams[key];
+        } else {
+          newParams[key] = value;
+        }
+      });
+      const path = generatePathWithSearchParams(loc.pathname, {
+        ...newParams,
+      });
+      navigate(path);
+    },
+    [loc.pathname, navigate, params]
+  );
+};
+
+export const useAppParam = <Key extends keyof ParamsState>(key: Key) => {
+  const val = useAppSelector((state) => state.params[key]);
+  return val as NonNullable<ParamsState[Key]> | undefined;
+};
