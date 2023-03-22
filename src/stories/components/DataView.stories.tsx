570e59e0c7b50f409c6956df3f2e2a784d59bfbc
Dakota Blair <dblair@bnl.gov>
2023-03-22 13:01:58 -0400

    Create testDataObjects fixture for DataView.

--- src/stories/components/DataView.stories.tsx
+++ src/stories/components/DataView.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import DataView from '../../common/components/DataView';
+import { testDataObjects } from '../../common/components/DataView.fixture';
 
 export default {
   title: 'Components/DataView',
@@ -21,22 +22,5 @@ export const Default = DataViewTemplate.bind({});
 
 Default.args = {
   wsId: 42,
-  dataObjects: [
-    {
-      name: 'fake genome',
-      obj_type: 'KBaseGenomes.Genome-1.0',
-    },
-    {
-      name: 'FBA_model_6000',
-      obj_type: 'KBaseFBA.FBAModel',
-    },
-    {
-      name: 'metagenome_200000000',
-      obj_type: 'Communities.Metagenome-2.0',
-    },
-    {
-      name: 'ridiculous_fake_type_that_shouldnt_exist',
-      obj_type: 'KBaseMuppets.Beeker-6.0',
-    },
-  ],
+  dataObjects: testDataObjects,
 };
