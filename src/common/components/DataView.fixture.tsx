d3c0bd466f45bf309671468adcc7cf7b5966c242
Dakota Blair <dblair@bnl.gov>
2023-03-22 13:01:58 -0400

    Create testDataObjects fixture for DataView.

---
+++ src/common/components/DataView.fixture.tsx
@@ -0,0 +1,18 @@
+export const testDataObjects = [
+  {
+    name: 'fake genome',
+    obj_type: 'KBaseGenomes.Genome-1.0',
+  },
+  {
+    name: 'FBA_model_6000',
+    obj_type: 'KBaseFBA.FBAModel',
+  },
+  {
+    name: 'metagenome_200000000',
+    obj_type: 'Communities.Metagenome-2.0',
+  },
+  {
+    name: 'ridiculous_fake_type_that_shouldnt_exist',
+    obj_type: 'KBaseMuppets.Beeker-6.0',
+  },
+];
--- src/common/components/DataView.test.tsx
+++ src/common/components/DataView.test.tsx
@@ -2,25 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Dataview from './DataView';
 import { Provider } from 'react-redux';
 import { createTestStore } from '../../app/store';
-
-const testDataObjects = [
-  {
-    name: 'fake genome',
-    obj_type: 'KBaseGenomes.Genome-1.0',
-  },
-  {
-    name: 'FBA_model_6000',
-    obj_type: 'KBaseFBA.FBAModel',
-  },
-  {
-    name: 'metagenome_200000000',
-    obj_type: 'Communities.Metagenome-2.0',
-  },
-  {
-    name: 'ridiculous_fake_type_that_shouldnt_exist',
-    obj_type: 'KBaseMuppets.Beeker-6.0',
-  },
-];
+import { testDataObjects } from './DataView.fixture';
 
 test('Dataview renders', () => {
   const { container } = render(
