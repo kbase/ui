a80becf1e85d6f54ee29dfa674aed02e225f3640
Dakota Blair <dblair@bnl.gov>
2023-04-13 09:50:17 -0400

    Add error boundaries in tests.
    When errors are thrown during a React render, jest will exit with an error
    message that is not verbose enough. By adding in error boundaries, these errors
    can be caught and dealt with, and if unexpected errors occur then more detailed
    error messages will be provided.

--- src/features/navigator/NarrativeView.test.tsx
+++ src/features/navigator/NarrativeView.test.tsx
@@ -1,7 +1,6 @@
 import {
   render,
   // screen
-  // screen
 } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { MemoryRouter as Router } from 'react-router-dom';
@@ -9,6 +8,11 @@ import { createTestStore } from '../../app/store';
 import { NarrativePreviewTemplate } from '../../stories/components/NarrativePreview.stories';
 import NarrativeView from './NarrativeView';
 import { testNarrative } from './NarrativeView.fixture';
+
+const consoleError = jest.spyOn(console, 'error');
+// This mockImplementation supresses console.error calls.
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+consoleError.mockImplementation(() => {});
 
 describe('The <NarrativeView /> component...', () => {
   test('renders.', () => {
@@ -25,6 +29,18 @@ describe('The <NarrativeView /> component...', () => {
 });
 
 describe('The <NarrativePreview /> component...', () => {
+  afterAll(() => {
+    consoleError.mockRestore();
+  });
+
+  afterEach(() => {
+    consoleError.mockClear();
+  });
+
+  beforeEach(() => {
+    consoleError.mockClear();
+  });
+
   test('renders.', () => {
     const { container } = render(
       <Router>
@@ -38,13 +54,23 @@ describe('The <NarrativePreview /> component...', () => {
     expect(container.querySelector('section.preview')).toBeInTheDocument();
   });
 
-  test('renders with more cells.', () => {
+  test('renders with one more cell.', () => {
     const { container } = render(
       <Router>
         <NarrativePreviewTemplate
           wsId={1}
           cells={testNarrative.cells.slice(0, 17)}
         />
+      </Router>
+    );
+    expect(container).toBeTruthy();
+    expect(container.querySelector('section.preview')).toBeInTheDocument();
+  });
+
+  test('renders with more cells.', () => {
+    const { container } = render(
+      <Router>
+        <NarrativePreviewTemplate wsId={1} cells={testNarrative.cells} />
       </Router>
     );
     expect(container).toBeTruthy();
