df6f7caab7a374d2cd4033a4e96ccb06f52b48d7
Dakota Blair <dblair@bnl.gov>
2023-05-04 14:47:40 -0400

    URO-86 Create Narrative Metadata Component
    This commit adds the <NarrativeMetadata /> component. It also adds tests for
    this component along with the <NarrativeView />, <NarrativePreview /> and
    <Navigator /> components. Error messages, warnings and logs are included in
    various undesireable situations, for example when a narrative has no cells. The
    stories have also been updated to make use of the updated fixtures.

--- src/features/navigator/NarrativeView.test.tsx
+++ src/features/navigator/NarrativeView.test.tsx
@@ -1,52 +1,179 @@
-import {
-  render,
-  // screen
-} from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import fetchMock, {
+  MockParams,
+  disableFetchMocks,
+  enableFetchMocks,
+} from 'jest-fetch-mock';
 import { Provider } from 'react-redux';
 import { MemoryRouter as Router } from 'react-router-dom';
 import { createTestStore } from '../../app/store';
 import { NarrativePreviewTemplate } from '../../stories/components/NarrativePreview.stories';
-import NarrativeView from './NarrativeView';
-import { testNarrative } from './NarrativeView.fixture';
+import { testResponseOKFactory } from './common';
+import {
+  initialTestState,
+  testNarrativeDoc,
+  testNarrativeDocsLookup,
+} from './fixtures';
+import NarrativeView, {
+  noPreviewMessage,
+  noWorkspaceCellsMessage,
+} from './NarrativeView';
 
-const consoleError = jest.spyOn(console, 'error');
-// This mockImplementation supresses console.error calls.
+const testResponseError: [string, MockParams] = [
+  JSON.stringify({
+    version: '2.0',
+    error: {
+      name: 'JSONRPCError',
+      code: -32500,
+      message:
+        'Object 1 cannot be accessed: No workspace with id 8675309 exists',
+      error: [
+        'us.kbase.workspace.database.exceptions.InaccessibleObjectException: ',
+        'Object 1 cannot be accessed: No workspace with id 8675309 exists',
+      ].join(''),
+    },
+  }),
+  { status: 500 },
+];
+
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-consoleError.mockImplementation(() => {});
+const emptyFunc = () => {};
+// NOTE: In this suite we supresses console error, log and warn calls.
+const consoleError = jest.spyOn(console, 'error');
+const consoleLog = jest.spyOn(console, 'log');
+const consoleWarn = jest.spyOn(console, 'warn');
+consoleError.mockImplementation(emptyFunc);
+consoleLog.mockImplementation(emptyFunc);
+consoleWarn.mockImplementation(emptyFunc);
 
 describe('The <NarrativeView /> component...', () => {
-  test('renders.', () => {
-    const { container } = render(
-      <Provider store={createTestStore()}>
-        <Router>
-          <NarrativeView narrativeUPA={'1/2/3'} view={'preview'} />
-        </Router>
-      </Provider>
+  beforeAll(() => {
+    enableFetchMocks();
+  });
+
+  afterAll(() => {
+    disableFetchMocks();
+  });
+
+  beforeEach(() => {
+    fetchMock.resetMocks();
+  });
+
+  test('renders.', async () => {
+    fetchMock.mockResponses(testResponseOKFactory(testNarrativeDoc));
+    const { container } = await waitFor(() =>
+      render(
+        <Provider store={createTestStore({ navigator: initialTestState })}>
+          <Router>
+            <NarrativeView narrativeUPA={'1/2/3'} view={'preview'} />
+          </Router>
+        </Provider>
+      )
     );
     expect(container).toBeTruthy();
     expect(container.querySelector('section.view')).toBeInTheDocument();
+  });
+
+  test('renders the data view.', async () => {
+    fetchMock.mockResponses(testResponseOKFactory(testNarrativeDoc));
+    const initialState = initialTestState;
+    const wsId = testNarrativeDoc.access_group;
+    const { container } = await waitFor(() =>
+      render(
+        <Provider
+          store={createTestStore({
+            navigator: initialState,
+          })}
+        >
+          <Router>
+            <NarrativeView narrativeUPA={`${wsId}/2/3`} view={'data'} />
+          </Router>
+        </Provider>
+      )
+    );
+    expect(container).toBeTruthy();
+    expect(container.querySelector('section.view')).toBeInTheDocument();
+  });
+
+  test('renders blank if narrative is not found.', async () => {
+    const wsId = 8675309;
+    fetchMock.mockResponses(testResponseError);
+    const { container } = await waitFor(() =>
+      render(
+        <Provider store={createTestStore({ navigator: initialTestState })}>
+          <Router>
+            <NarrativeView narrativeUPA={`${wsId}/2/3`} view={'data'} />
+          </Router>
+        </Provider>
+      )
+    );
+    expect(wsId in testNarrativeDocsLookup).toBeFalsy();
+    expect(container).toBeTruthy();
+    expect(container.querySelector('section.view')).toBeInTheDocument();
+  });
+
+  test('warns if no cells are found in narrative.', async () => {
+    const narrativeDoc = initialTestState.narrativeDocs[0];
+    fetchMock.mockResponses(testResponseOKFactory(narrativeDoc));
+    const wsId = narrativeDoc.access_group;
+    const testStore = createTestStore({ navigator: initialTestState });
+    const { container } = await waitFor(() =>
+      render(
+        <Provider store={testStore}>
+          <Router>
+            <NarrativeView narrativeUPA={`${wsId}/2/3`} view={'preview'} />
+          </Router>
+        </Provider>
+      )
+    );
+    expect(container).toBeTruthy();
+    expect(container.querySelector('section.preview')).toBeInTheDocument();
+    expect(
+      screen.getByText(noPreviewMessage, {
+        exact: false,
+      })
+    );
+    const lastCallArg = consoleLog.mock.lastCall
+      ? consoleLog.mock.lastCall[0]
+      : '';
+    expect(lastCallArg).toBe(noWorkspaceCellsMessage);
   });
 });
 
 describe('The <NarrativePreview /> component...', () => {
   afterAll(() => {
     consoleError.mockRestore();
-  });
-
-  afterEach(() => {
-    consoleError.mockClear();
+    consoleLog.mockRestore();
+    consoleWarn.mockRestore();
   });
 
   beforeEach(() => {
     consoleError.mockClear();
+    consoleLog.mockClear();
+    consoleWarn.mockClear();
   });
 
   test('renders.', () => {
     const { container } = render(
       <Router>
         <NarrativePreviewTemplate
+          cells={testNarrativeDoc.cells.slice(0, 16)}
+          narrativeDoc={testNarrativeDoc}
           wsId={1}
-          cells={testNarrative.cells.slice(0, 16)}
+        />
+      </Router>
+    );
+    expect(container).toBeTruthy();
+    expect(container.querySelector('section.preview')).toBeInTheDocument();
+  });
+
+  test('renders with no cells.', () => {
+    const { container } = render(
+      <Router>
+        <NarrativePreviewTemplate
+          cells={[]}
+          narrativeDoc={testNarrativeDoc}
+          wsId={1}
         />
       </Router>
     );
@@ -58,8 +185,9 @@ describe('The <NarrativePreview /> component...', () => {
     const { container } = render(
       <Router>
         <NarrativePreviewTemplate
+          cells={testNarrativeDoc.cells.slice(0, 17)}
+          narrativeDoc={testNarrativeDoc}
           wsId={1}
-          cells={testNarrative.cells.slice(0, 17)}
         />
       </Router>
     );
@@ -70,7 +198,11 @@ describe('The <NarrativePreview /> component...', () => {
   test('renders with more cells.', () => {
     const { container } = render(
       <Router>
-        <NarrativePreviewTemplate wsId={1} cells={testNarrative.cells} />
+        <NarrativePreviewTemplate
+          narrativeDoc={testNarrativeDoc}
+          cells={testNarrativeDoc.cells}
+          wsId={1}
+        />
       </Router>
     );
     expect(container).toBeTruthy();
--- src/stories/components/NarrativePreview.stories.tsx
+++ src/stories/components/NarrativePreview.stories.tsx
@@ -2,7 +2,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Provider } from 'react-redux';
 import { createTestStore } from '../../app/store';
 import { NarrativePreview } from '../../features/navigator/NarrativeView';
-import { testNarrative } from '../../features/navigator/NarrativeView.fixture';
+import { testNarrativeDoc } from '../../features/navigator/fixtures';
 
 export const NarrativePreviewTemplate: ComponentStory<typeof NarrativePreview> =
   (args) => {
@@ -16,11 +16,13 @@ export const NarrativePreviewTemplate: ComponentStory<typeof NarrativePreview> =
 export const Default = NarrativePreviewTemplate.bind({});
 
 Default.args = {
-  wsId: 42,
-  cells: testNarrative.cells,
+  cells: testNarrativeDoc.cells,
+  narrativeDoc: testNarrativeDoc,
+  wsId: testNarrativeDoc.access_group,
 };
 
 export default {
   title: 'Components/NarrativePreview',
   component: NarrativePreview,
+  excludeStories: /NarrativePreviewTemplate/,
 } as ComponentMeta<typeof NarrativePreview>;
