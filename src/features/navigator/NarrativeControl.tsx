dda85108855928f88b27f0021ec75c01fe997f07
Dakota Blair <dblair@bnl.gov>
2023-05-16 17:10:13 -0400

    WIP003

--- src/features/navigator/NarrativeControl.tsx
+++ src/features/navigator/NarrativeControl.tsx
@@ -1,26 +1,97 @@
 import { FC } from 'react';
 import { useParams } from 'react-router-dom';
+import { Button, Dropdown } from '../../common/components';
 import { NarrativeDoc } from '../../common/types/NarrativeDoc';
 import classes from './NarrativeControl.module.scss';
 
-const ControlLatest: FC<{}> = (props) => {
+interface ControlProps {
+  narrativeDoc: NarrativeDoc;
+}
+
+const getDeleteDialog = () => {
+  let dialog: HTMLDialogElement | null = document.querySelector(
+    `.${classes['narrative-control']}.${classes.delete}`
+  );
+  if (!dialog) {
+    dialog = document.createElement('dialog');
+    document.body.appendChild(dialog);
+  }
+  return dialog;
+};
+
+const deleteDialogToggle = () => {
+  const dialog = getDeleteDialog();
+  if (dialog.open) {
+    return dialog.close();
+  }
+  console.log({ dialog }); // eslint-disable-line no-console
+  return dialog.showModal();
+};
+
+const DeleteDialog: FC = () => {
+  const closeHandler = () => {
+    const dialog = getDeleteDialog();
+    dialog.close();
+  };
+  return (
+    <dialog className={`${classes['narrative-control']} ${classes.delete}`}>
+      Are you sure you want to delete this narrative?
+      <Button onClick={closeHandler}>Close Dialog</Button>
+      <Button>Yes</Button>
+      <Button onClick={closeHandler}>No</Button>
+    </dialog>
+  );
+  // return <div onClick={clickHandler}>Delete {dialog} </div>;
+};
+
+const controlLatestOptions = [
+  <li>Manage Sharing</li>,
+  <li>Copy this Narrative</li>,
+  <li>Rename</li>,
+  <li>Link to Organization</li>,
+  <div onClick={deleteDialogToggle}>Delete</div>,
+].map((option) => ({
+  value: '',
+  icon: undefined,
+  label: option,
+}));
+
+const ControlLatest: FC<ControlProps> = ({ narrativeDoc }) => {
   return (
     <>
-      <li>Manage Sharing</li>
-      <li>Copy this Narrative</li>
-      <li>Rename</li>
-      <li>Link to Organization</li>
-      <li>Delete</li>
+      <Dropdown
+        horizontalMenuAlign={'right'}
+        options={[{ options: controlLatestOptions }]}
+        onChange={(opt) => {
+          console.log({ opt }); // eslint-disable-line no-console
+        }}
+      >
+        <div>Latest</div>
+      </Dropdown>
+      <DeleteDialog />
     </>
   );
 };
 
-const ControlPrevious: FC<{}> = (props) => {
+const controlPreviousOptions = [<li>Copy</li>, <li>Restore</li>].map(
+  (option) => ({
+    value: '',
+    icon: undefined,
+    label: <div>{option} </div>,
+  })
+);
+
+const ControlPrevious: FC<ControlProps> = ({ narrativeDoc }) => {
   return (
-    <>
-      <li>Copy</li>
-      <li>Restore</li>
-    </>
+    <Dropdown
+      horizontalMenuAlign={'right'}
+      options={[{ options: controlPreviousOptions }]}
+      onChange={(opt) => {
+        console.log({ opt }); // eslint-disable-line no-console
+      }}
+    >
+      <div>Previous</div>
+    </Dropdown>
   );
 };
 
@@ -28,32 +99,14 @@ const NarrativeControl: FC<{ narrativeDoc: NarrativeDoc }> = ({
   narrativeDoc,
 }) => {
   const { ver } = useParams();
-  const { access_group: wsId, version } = narrativeDoc;
-  const dialog = (
-    <>
-      <dialog className={classes['narrative-control']}>
-        <ul>
-          {ver === version.toString() ? <ControlLatest /> : <ControlPrevious />}
-        </ul>
-      </dialog>
-    </>
-  );
-  const clickHandler = () => {
-    const dialog: HTMLDialogElement | null = document.querySelector(
-      `.${classes['narrative-control']}`
-    );
-    if (!dialog) {
-      return;
-    }
-    if (dialog.open) {
-      return dialog.close();
-    }
-    return dialog.showModal();
-  };
+  const { version } = narrativeDoc;
   return (
     <>
-      <span onClick={clickHandler}>{wsId}</span>
-      {dialog}
+      {ver === version.toString() ? (
+        <ControlLatest narrativeDoc={narrativeDoc} />
+      ) : (
+        <ControlPrevious narrativeDoc={narrativeDoc} />
+      )}
     </>
   );
 };
