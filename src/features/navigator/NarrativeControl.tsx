84c65c1f654d4fa659b9d39dd0bb6e60d344b8f0
Dakota Blair <dblair@bnl.gov>
2023-06-02 16:31:15 -0400

    WIP007

--- src/features/navigator/NarrativeControl.tsx
+++ src/features/navigator/NarrativeControl.tsx
@@ -4,28 +4,36 @@ import { ModalContext } from '../../app/App';
 import { Button, Dropdown } from '../../common/components';
 import { NarrativeDoc } from '../../common/types/NarrativeDoc';
 
-interface ControlProps {
+interface ControlLatestProps {
   narrativeDoc: NarrativeDoc;
 }
 
-const Delete: FC<{ no: () => void }> = ({ no }) => {
+const Delete: FC<{ no: () => void; yes: () => void }> = ({ no, yes }) => {
   return (
     <>
-      Are you sure you want to delete this narrative?
-      <Button>Yes</Button>
-      <Button onClick={no}>No</Button>
+      <p>Deleting a Narrative will permanently remove it and all its data.</p>
+      <p>This action cannot be undone!</p>
+      <p>Continue?</p>
+      <div>
+        <Button onClick={yes}>Delete</Button>
+        <Button onClick={no}>Cancel</Button>
+      </div>
     </>
   );
 };
 
-interface ControlResponses {
+interface ControlLatestResponses {
   deleteNo: () => void;
+  deleteYes: () => void;
 }
 
-const controlLatestDialogsFactory = ({ deleteNo }: ControlResponses) => {
+const controlLatestDialogsFactory = ({
+  deleteNo,
+  deleteYes,
+}: ControlLatestResponses) => {
   const dialogs: Record<string, JSX.Element> = {
     'Copy this Narrative': <>Copy this Narrative</>,
-    Delete: <Delete no={deleteNo} />,
+    Delete: <Delete no={deleteNo} yes={deleteYes} />,
     'Link to Organization': <>Link to Organization</>,
     'Manage Sharing': <>Manage Sharing</>,
     Rename: <>Rename</>,
@@ -45,11 +53,26 @@ const controlLatestOptions = [
   label: <li>{option}</li>,
 }));
 
-const ControlLatest: FC<ControlProps> = ({ narrativeDoc }) => {
+const deleteNarrative = async (wsId: number) => {
+  const message = `Delete ${wsId}.`;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // eslint-disable-next-line no-console
+      console.log(message);
+      resolve(message);
+    }, 1000);
+  });
+};
+
+const ControlLatest: FC<ControlLatestProps> = ({ narrativeDoc }) => {
   const { getModalControls, setModalContents } = useContext(ModalContext);
   const { modalClose } = getModalControls();
   const controlLatestDialogs = controlLatestDialogsFactory({
     deleteNo: modalClose,
+    deleteYes: async () => {
+      await deleteNarrative(narrativeDoc.access_group);
+      modalClose();
+    },
   });
   const closeHandler = () => {
     modalClose();
@@ -75,21 +98,96 @@ const ControlLatest: FC<ControlProps> = ({ narrativeDoc }) => {
   );
 };
 
-const controlPreviousOptions = [<li>Copy</li>, <li>Restore</li>].map(
+const Restore: FC<{ no: () => void; yes: () => void }> = ({ no, yes }) => {
+  const version = 0;
+  return (
+    <>
+      <p>
+        Reverting a narrative will create a new version identical to v{version}.
+      </p>
+
+      <p>
+        This new narrative can be reverted to an earlier version at any time.
+      </p>
+
+      <p>Do you wish to continue?</p>
+
+      <div>
+        <Button onClick={yes}>Revert</Button>
+        <Button onClick={no}>Cancel</Button>
+      </div>
+    </>
+  );
+};
+
+interface ControlPreviousResponses {
+  restoreNo: () => void;
+  restoreYes: () => void;
+}
+
+const controlPreviousDialogsFactory = ({
+  restoreNo,
+  restoreYes,
+}: ControlPreviousResponses) => {
+  const dialogs: Record<string, JSX.Element> = {
+    'Copy this version': <>Copy</>,
+    'Restore Version': <Restore no={restoreNo} yes={restoreYes} />,
+  };
+  return dialogs;
+};
+
+const controlPreviousOptions = ['Copy this version', 'Restore Version'].map(
   (option) => ({
-    value: '',
+    value: option,
     icon: undefined,
-    label: <div>{option} </div>,
+    label: <li>{option}</li>,
   })
 );
 
-const ControlPrevious: FC<ControlProps> = ({ narrativeDoc }) => {
+const restoreNarrative = async (wsId: number, version: number) => {
+  const message = `Restore ${wsId}.`;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // eslint-disable-next-line no-console
+      console.log(message);
+      resolve(message);
+    }, 1000);
+  });
+};
+
+interface ControlPreviousProps {
+  narrativeDoc: NarrativeDoc;
+  version: number;
+}
+
+const ControlPrevious: FC<ControlPreviousProps> = ({
+  narrativeDoc,
+  version,
+}) => {
+  const { getModalControls, setModalContents } = useContext(ModalContext);
+  const { modalClose } = getModalControls();
+  const controlPreviousDialogs = controlPreviousDialogsFactory({
+    restoreNo: modalClose,
+    restoreYes: async () => {
+      await restoreNarrative(narrativeDoc.access_group, Number(version));
+      modalClose();
+    },
+  });
+  const closeHandler = () => {
+    modalClose();
+  };
+  const closeButton = <Button onClick={closeHandler}>Close Dialog</Button>;
   return (
     <Dropdown
       horizontalMenuAlign={'right'}
       options={[{ options: controlPreviousOptions }]}
       onChange={(opt) => {
-        console.log({ opt }); // eslint-disable-line no-console
+        setModalContents(
+          <span>
+            {controlPreviousDialogs[opt[0].value]}
+            {closeButton}
+          </span>
+        );
       }}
     >
       <div>Previous</div>
@@ -102,12 +200,13 @@ const NarrativeControl: FC<{ narrativeDoc: NarrativeDoc }> = ({
 }) => {
   const { ver } = useParams();
   const { version } = narrativeDoc;
+  // TODO: Do something about bad version values, but higher up.
   return (
     <>
       {!ver || ver === version.toString() ? (
         <ControlLatest narrativeDoc={narrativeDoc} />
       ) : (
-        <ControlPrevious narrativeDoc={narrativeDoc} />
+        <ControlPrevious narrativeDoc={narrativeDoc} version={Number(ver)} />
       )}
     </>
   );
