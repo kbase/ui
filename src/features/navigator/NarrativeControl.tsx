b13fd527cc94930cf5d9dda3e0c0c8b81fbebc92
Dakota Blair <dblair@bnl.gov>
2023-06-07 16:50:52 -0400

    WIP011

--- src/features/navigator/NarrativeControl.tsx
+++ src/features/navigator/NarrativeControl.tsx
@@ -1,24 +1,100 @@
 import { FC, useContext } from 'react';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 import { ModalContext } from '../../app/App';
 import { Button, Dropdown } from '../../common/components';
+import { inputRegisterFactory } from '../../common/components/Input.common';
+import { Input } from '../../common/components/Input';
 import { NarrativeDoc } from '../../common/types/NarrativeDoc';
 import { normalizeVersion } from './common';
 
 // See also https://github.com/kbaseapps/NarrativeService/blob/main/lib/NarrativeService/NarrativeManager.py#L9
-// const MAX_WS_METADATA_VALUE_SIZE = 900;
+const MAX_WS_METADATA_VALUE_SIZE = 900;
 
 interface ControlLatestProps {
   narrativeDoc: NarrativeDoc;
 }
 
-const Copy: FC<{ no: () => void; yes: () => void }> = ({ no, yes }) => {
+const Copy: FC<{
+  narrativeDoc: NarrativeDoc;
+  no: () => void;
+  version: number;
+  yes: () => void;
+}> = ({ narrativeDoc, no, version, yes }) => {
+  interface CopyValues {
+    narrativeCopyName: string;
+  }
+
+  const { formState, register } = useForm<CopyValues>({
+    defaultValues: {
+      narrativeCopyName: `${narrativeDoc.narrative_title} - Copy`,
+    },
+    mode: 'all',
+  });
+  const inputRegister = inputRegisterFactory<CopyValues>({
+    formState,
+    register,
+  });
   return (
     <>
-      <p>Make a Copy</p>
+      <p>
+        {version < narrativeDoc.version
+          ? 'Make a copy of this version '
+          : 'Make a Copy'}
+      </p>
       <p>Enter a name for the new Narrative.</p>
       <div>
-        <Button onClick={yes}>Delete</Button>
+        <Input
+          label={<>New Narrative Title</>}
+          {...inputRegister('narrativeCopyName', {
+            maxLength: {
+              value: MAX_WS_METADATA_VALUE_SIZE,
+              message: 'too long',
+            },
+          })}
+        />
+        <Button onClick={yes}>OK</Button>
+        <Button onClick={no}>Cancel</Button>
+      </div>
+    </>
+  );
+};
+
+const Rename: FC<{
+  narrativeDoc: NarrativeDoc;
+  no: () => void;
+  yes: () => void;
+}> = ({ narrativeDoc, no, yes }) => {
+  interface RenameValues {
+    narrativeRenameName: string;
+  }
+
+  const { formState, register } = useForm<RenameValues>({
+    defaultValues: {
+      narrativeRenameName: narrativeDoc.narrative_title,
+    },
+    mode: 'all',
+  });
+  const inputRegister = inputRegisterFactory<RenameValues>({
+    formState,
+    register,
+  });
+  return (
+    <>
+      <p>Rename Narrative</p>
+      <p>Enter a new name for the Narrative:</p>
+
+      <div>
+        <Input
+          label={<>New Narrative Title</>}
+          {...inputRegister('narrativeRenameName', {
+            maxLength: {
+              value: MAX_WS_METADATA_VALUE_SIZE,
+              message: 'too long',
+            },
+          })}
+        />
+        <Button onClick={yes}>OK</Button>
         <Button onClick={no}>Cancel</Button>
       </div>
     </>
@@ -40,21 +116,42 @@ const Delete: FC<{ no: () => void; yes: () => void }> = ({ no, yes }) => {
   );
 };
 
-interface ControlLatestResponses {
+interface ControlLatestParams {
+  copyNo: () => void;
+  copyYes: () => void;
   deleteNo: () => void;
   deleteYes: () => void;
+  renameNo: () => void;
+  renameYes: () => void;
+  narrativeDoc: NarrativeDoc;
+  version: number;
 }
 
 const controlLatestDialogsFactory = ({
+  copyNo,
+  copyYes,
   deleteNo,
   deleteYes,
-}: ControlLatestResponses) => {
+  narrativeDoc,
+  renameNo,
+  renameYes,
+  version,
+}: ControlLatestParams) => {
   const dialogs: Record<string, JSX.Element> = {
-    'Copy this Narrative': <>Copy this Narrative</>,
+    'Copy this Narrative': (
+      <Copy
+        narrativeDoc={narrativeDoc}
+        no={copyNo}
+        version={version}
+        yes={copyYes}
+      />
+    ),
     Delete: <Delete no={deleteNo} yes={deleteYes} />,
     'Link to Organization': <>Link to Organization</>,
     'Manage Sharing': <>Manage Sharing</>,
-    Rename: <>Rename</>,
+    Rename: (
+      <Rename narrativeDoc={narrativeDoc} no={renameNo} yes={renameYes} />
+    ),
   };
   return dialogs;
 };
@@ -67,12 +164,33 @@ const controlLatestOptions = [
   'Delete',
 ].map((option) => ({
   value: option,
-  icon: undefined,
   label: <li>{option}</li>,
 }));
 
+const copyNarrative = async (wsId: number, version: number, name: string) => {
+  const message = `Copy version ${version} of ${wsId} with name ${name}.`;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // eslint-disable-next-line no-console
+      console.log(message);
+      resolve(message);
+    }, 1000);
+  });
+};
+
 const deleteNarrative = async (wsId: number) => {
   const message = `Delete ${wsId}.`;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // eslint-disable-next-line no-console
+      console.log(message);
+      resolve(message);
+    }, 1000);
+  });
+};
+
+const renameNarrative = async (wsId: number, name: string) => {
+  const message = `Rename ${wsId} to ${name}.`;
   return new Promise((resolve) => {
     setTimeout(() => {
       // eslint-disable-next-line no-console
@@ -86,11 +204,23 @@ const ControlLatest: FC<ControlLatestProps> = ({ narrativeDoc }) => {
   const { getModalControls, setModalContents } = useContext(ModalContext);
   const { modalClose } = getModalControls();
   const controlLatestDialogs = controlLatestDialogsFactory({
+    copyNo: modalClose,
+    copyYes: async () => {
+      await copyNarrative(narrativeDoc.access_group, 1, 'new-name');
+      modalClose();
+    },
     deleteNo: modalClose,
     deleteYes: async () => {
       await deleteNarrative(narrativeDoc.access_group);
       modalClose();
     },
+    renameNo: modalClose,
+    renameYes: async () => {
+      await renameNarrative(narrativeDoc.access_group, 'new-name');
+      modalClose();
+    },
+    version: narrativeDoc.version,
+    narrativeDoc,
   });
   const closeHandler = () => {
     modalClose();
@@ -116,8 +246,11 @@ const ControlLatest: FC<ControlLatestProps> = ({ narrativeDoc }) => {
   );
 };
 
-const Restore: FC<{ no: () => void; yes: () => void }> = ({ no, yes }) => {
-  const version = 0;
+const Restore: FC<{ no: () => void; version: number; yes: () => void }> = ({
+  no,
+  version,
+  yes,
+}) => {
   return (
     <>
       <p>
@@ -138,11 +271,13 @@ const Restore: FC<{ no: () => void; yes: () => void }> = ({ no, yes }) => {
   );
 };
 
-interface ControlPreviousResponses {
+interface ControlPreviousParams {
   copyNo: () => void;
   copyYes: () => void;
+  narrativeDoc: NarrativeDoc;
   restoreNo: () => void;
   restoreYes: () => void;
+  version: number;
 }
 
 const controlPreviousDialogsFactory = ({
@@ -150,10 +285,21 @@ const controlPreviousDialogsFactory = ({
   copyYes,
   restoreNo,
   restoreYes,
-}: ControlPreviousResponses) => {
+  narrativeDoc,
+  version,
+}: ControlPreviousParams) => {
   const dialogs: Record<string, JSX.Element> = {
-    'Copy this version': <Copy no={copyNo} yes={copyYes} />,
-    'Restore Version': <Restore no={restoreNo} yes={restoreYes} />,
+    'Copy this version': (
+      <Copy
+        narrativeDoc={narrativeDoc}
+        no={copyNo}
+        version={version}
+        yes={copyYes}
+      />
+    ),
+    'Restore Version': (
+      <Restore no={restoreNo} version={version} yes={restoreYes} />
+    ),
   };
   return dialogs;
 };
@@ -161,7 +307,6 @@ const controlPreviousDialogsFactory = ({
 const controlPreviousOptions = ['Copy this version', 'Restore Version'].map(
   (option) => ({
     value: option,
-    icon: undefined,
     label: <li>{option}</li>,
   })
 );
@@ -190,12 +335,17 @@ const ControlPrevious: FC<ControlPreviousProps> = ({
   const { modalClose } = getModalControls();
   const controlPreviousDialogs = controlPreviousDialogsFactory({
     copyNo: modalClose,
-    copyYes: modalClose,
+    copyYes: async () => {
+      await copyNarrative(narrativeDoc.access_group, 1, 'new-name');
+      modalClose();
+    },
     restoreNo: modalClose,
     restoreYes: async () => {
       await restoreNarrative(narrativeDoc.access_group, Number(version));
       modalClose();
     },
+    narrativeDoc,
+    version,
   });
   const closeHandler = () => {
     modalClose();
