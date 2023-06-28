c7495b6c0b87a969c4ea6e5d4106f1d8007a2123
David Lyon <dauglyon@users.noreply.github.com>
2023-06-28 12:25:36 -0700

    Added GHA, entrypoint, and hooked it together

--- src/common/components/PlaceholderFactory.tsx
+++ src/common/components/PlaceholderFactory.tsx
@@ -12,7 +12,7 @@ export const PlaceholderFactory = (name: string) => {
   const Placeholder: FC<PlaceholderInterface> = (props) => {
     const invalidEnvironment =
       process.env.NODE_ENV === 'production' &&
-      process.env.REACT_APP_KBASE_ENV !== 'ci-europa';
+      process.env.REACT_APP_KBASE_DOMAIN !== 'ci-europa.kbase.us';
     if (invalidEnvironment) {
       throw new Error('Placeholder components may not be used in production.');
     }
