9e708e7fa3f2fdc8aca39753c6a2c84f4c8a1343
Cody O'Donnell <ctodonnell@lbl.gov>
2023-07-05 15:16:48 -0700

    Update button component to accept styles, add LinkButton, update tab and other navigator styles

--- src/common/components/Button.tsx
+++ src/common/components/Button.tsx
@@ -1,7 +1,35 @@
 import { FunctionComponent as FC } from 'react';
 import classes from './Button.module.scss';
 
-export const Button: FC<React.ComponentProps<'button'>> = (props) => {
-  const className = [classes.button, props.className].join(' ');
-  return <button className={className} type={undefined} {...props} />;
+const colorClasses = Object.keys(classes).filter((d) => d !== 'button');
+const colors = [...colorClasses] as const;
+type Color = typeof colors[number];
+
+export interface CommonButtonProps {
+  variant?: 'contained' | 'outlined' | 'text';
+  color?: Color;
+  icon?: React.ReactNode;
+}
+
+interface ButtonProps
+  extends CommonButtonProps,
+    React.ComponentProps<'button'> {}
+
+export const Button: FC<ButtonProps> = ({
+  variant = 'contained',
+  color = 'white',
+  className,
+  ...props
+}) => {
+  const classNames = [
+    classes.button,
+    className,
+    classes[`button--${color}`],
+  ].join(' ');
+  return (
+    <button className={classNames} {...props}>
+      {props.icon && <span className={classes.icon}>{props.icon}</span>}
+      {props.children}
+    </button>
+  );
 };
