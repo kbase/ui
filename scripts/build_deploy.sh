50f296ae88efaa7656fd6de2f90f8b452f91202a
Dakota Blair <dblair@bnl.gov>
2023-07-06 14:45:17 -0400

    Update build_deploy.sh

--- scripts/build_deploy.sh
+++ scripts/build_deploy.sh
@@ -2,10 +2,11 @@
 
 
 declare -a enviroments=(
-# "<name>         <domain>                <legacy-domain>"
-  "ci             ci.kbase.us             legacy.kbase.us"
-  "ci-europa      ci-europa.kbase.us      legacy.ci-europa.kbase.us"
-  "narrative-dev  narrative-dev.kbase.us  legacy.narrative-dev.kbase.us"
+# "<name>         <domain>                <legacy-domain>               <public-url>"
+  "ci             ci.kbase.us             legacy.kbase.us                /"
+  "ci-europa      ci-europa.kbase.us      legacy.ci-europa.kbase.us      /"
+  "localhost      localhost               legacy.ci-europa.kbase.us      /dev"
+  "narrative-dev  narrative-dev.kbase.us  legacy.narrative-dev.kbase.us  /"
 )
 
 for enviro in "${enviroments[@]}"; do
@@ -15,6 +16,7 @@ for enviro in "${enviroments[@]}"; do
   BUILD_PATH="./deploy/${strarr[0]}" \
   REACT_APP_KBASE_DOMAIN="${strarr[1]}" \
   REACT_APP_KBASE_LEGACY_DOMAIN="${strarr[2]}" \
+  PUBLIC_URL="${strarr[3]}" \
   npm run build && \
   echo "Built static files for enviroment \"${strarr[0]}\".";
 done
