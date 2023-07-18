bca8ba9f52f40bc9c864b5127ebcda120ff4a65f
Cody O'Donnell <ctodonnell@lbl.gov>
2023-07-18 15:08:33 -0700

    Modify metadata section of narrative view

---
+++ src/common/components/LabelValueTable.tsx
@@ -0,0 +1,31 @@
+import { ComponentProps, ReactElement, FC } from 'react';
+
+import classes from './LabelValueTable.module.scss';
+
+interface LabelValuePair {
+  label: string;
+  value: string | number | ReactElement;
+}
+
+interface LabelValueTableProps extends ComponentProps<'table'> {
+  data?: LabelValuePair[];
+}
+
+/**
+ * Component for displaying a flat list of label-value pairs
+ * in a two column table.
+ */
+export const LabelValueTable: FC<LabelValueTableProps> = (props) => {
+  const { className, data } = props;
+
+  return (
+    <table className={`${classes['label-value-table']} ${className || ''}`}>
+      {data?.map((row, i) => (
+        <tr>
+          <td>{row.label}</td>
+          <td>{row.value}</td>
+        </tr>
+      ))}
+    </table>
+  );
+};
