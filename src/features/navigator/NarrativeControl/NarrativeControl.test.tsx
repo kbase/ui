3d5a48e84661820fc067e1125e061fdedd05efec
Dakota Blair <dblair@bnl.gov>
2023-08-09 16:14:56 -0400

    Add tests for narrative control components.
    This also silences expected error logs in the Table component tests.

---
+++ src/features/navigator/NarrativeControl/NarrativeControl.test.tsx
@@ -0,0 +1,111 @@
+// Tests for <NarrativeControl />
+
+import { render, screen } from '@testing-library/react';
+import { emptyFunction } from '../common';
+import { testNarrativeDoc } from '../fixtures';
+import {
+  CopyTemplate,
+  DeleteTemplate,
+  LinkTemplate,
+  NarrativeControlTemplate,
+  RenameTemplate,
+  RestoreTemplate,
+  ShareTemplate,
+} from './NarrativeControl.stories';
+
+describe('The <NarrativeControl /> component...', () => {
+  test('renders.', () => {
+    const { container } = render(
+      <NarrativeControlTemplate narrativeDoc={testNarrativeDoc} />
+    );
+    expect(container).toBeTruthy();
+    expect(screen.getByText('Latest', { exact: false })).toBeInTheDocument();
+  });
+});
+
+describe('The <Copy /> component...', () => {
+  test('renders.', () => {
+    const { container } = render(
+      <CopyTemplate
+        modalClose={emptyFunction}
+        narrativeDoc={testNarrativeDoc}
+        version={1}
+      />
+    );
+    expect(container).toBeTruthy();
+    expect(
+      screen.getByText('Make a Copy', { exact: false })
+    ).toBeInTheDocument();
+  });
+});
+
+describe('The <Delete /> component...', () => {
+  test('renders.', () => {
+    const { container } = render(
+      <DeleteTemplate
+        modalClose={emptyFunction}
+        narrativeDoc={testNarrativeDoc}
+      />
+    );
+    expect(container).toBeTruthy();
+    expect(
+      screen.getByText('Delete Narrative', { exact: false })
+    ).toBeInTheDocument();
+  });
+});
+
+describe('The <Link /> component...', () => {
+  test('renders.', () => {
+    const { container } = render(
+      <LinkTemplate
+        modalClose={emptyFunction}
+        narrativeDoc={testNarrativeDoc}
+      />
+    );
+    expect(container).toBeTruthy();
+  });
+});
+
+describe('The <Rename /> component...', () => {
+  test('renders.', () => {
+    const { container } = render(
+      <RenameTemplate
+        modalClose={emptyFunction}
+        narrativeDoc={testNarrativeDoc}
+      />
+    );
+    expect(container).toBeTruthy();
+    expect(
+      screen.getByText('Rename Narrative', { exact: false })
+    ).toBeInTheDocument();
+  });
+});
+
+describe('The <Restore /> component...', () => {
+  test('renders.', () => {
+    const { container } = render(
+      <RestoreTemplate
+        modalClose={emptyFunction}
+        narrativeDoc={testNarrativeDoc}
+        version={1}
+      />
+    );
+    expect(container).toBeTruthy();
+    expect(screen.getByText('Reverting', { exact: false })).toBeInTheDocument();
+  });
+});
+
+describe('The <Share /> component...', () => {
+  test('renders.', () => {
+    const { container } = render(
+      <ShareTemplate
+        modalClose={emptyFunction}
+        narrativeDoc={testNarrativeDoc}
+      />
+    );
+    expect(container).toBeTruthy();
+    expect(
+      screen.getByText('Manage Sharing', { exact: false })
+    ).toBeInTheDocument();
+  });
+});
