7a43df8a09966e34a341ddb84fd9096085db2068
Dakota Blair <dblair@bnl.gov>
2023-08-09 16:08:52 -0400

    Add the NarrativeControl component.
    The NarrativeControl component has different views depending on whether the
    selected version is the latest or not. When the version selected is the latest
    then the following components are enabled:
    - Copy
    - Delete
    - Link
    - Rename
    - Share
    When the version selected is a previous version:
    - Copy
    - Restore

---
+++ src/features/navigator/NarrativeControl/index.tsx
@@ -0,0 +1,141 @@
+import { FC, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { ModalContext } from '../../../app/App';
+import { Button, Dropdown } from '../../../common/components';
+import { NarrativeDoc } from '../../../common/types/NarrativeDoc';
+import { normalizeVersion } from '../common';
+import { Copy } from './Copy';
+import { Delete } from './Delete';
+import { Link as LinkOrg } from './Link';
+import { Restore } from './Restore';
+import { Rename } from './Rename';
+import { Share } from './Share';
+
+const controlLatestOptions = [
+  'Manage Sharing',
+  'Copy this Narrative',
+  'Rename',
+  'Link to Organization',
+  'Delete',
+].map((option) => ({
+  value: option,
+  label: <li>{option}</li>,
+}));
+
+interface ControlLatestProps {
+  narrativeDoc: NarrativeDoc;
+}
+
+const ControlLatest: FC<ControlLatestProps> = ({ narrativeDoc }) => {
+  const { getModalControls, setModalContents } = useContext(ModalContext);
+  const { modalClose } = getModalControls();
+  const { version } = narrativeDoc;
+  const controlLatestDialogs: Record<string, JSX.Element> = {
+    'Copy this Narrative': (
+      <Copy
+        narrativeDoc={narrativeDoc}
+        modalClose={modalClose}
+        version={version}
+      />
+    ),
+    Delete: <Delete narrativeDoc={narrativeDoc} modalClose={modalClose} />,
+    'Link to Organization': (
+      <LinkOrg narrativeDoc={narrativeDoc} modalClose={modalClose} />
+    ),
+    'Manage Sharing': (
+      <Share narrativeDoc={narrativeDoc} modalClose={modalClose} />
+    ),
+    Rename: <Rename narrativeDoc={narrativeDoc} modalClose={modalClose} />,
+  };
+  return (
+    <>
+      <Dropdown
+        horizontalMenuAlign={'right'}
+        options={[{ options: controlLatestOptions }]}
+        onChange={(opt) => {
+          setModalContents(
+            <span>
+              {controlLatestDialogs[opt[0].value]}
+              <Button onClick={modalClose}>Close Dialog</Button>
+            </span>
+          );
+        }}
+      >
+        <div>Latest</div>
+      </Dropdown>
+    </>
+  );
+};
+
+const controlPreviousOptions = ['Copy this version', 'Restore Version'].map(
+  (option) => ({
+    value: option,
+    label: <li>{option}</li>,
+  })
+);
+
+interface ControlPreviousProps {
+  narrativeDoc: NarrativeDoc;
+  version: number;
+}
+
+const ControlPrevious: FC<ControlPreviousProps> = ({
+  narrativeDoc,
+  version,
+}) => {
+  const { getModalControls, setModalContents } = useContext(ModalContext);
+  const { modalClose } = getModalControls();
+  const controlPreviousDialogs: Record<string, JSX.Element> = {
+    'Copy this version': (
+      <Copy
+        narrativeDoc={narrativeDoc}
+        modalClose={modalClose}
+        version={version}
+      />
+    ),
+    'Restore Version': (
+      <Restore
+        modalClose={modalClose}
+        narrativeDoc={narrativeDoc}
+        version={version}
+      />
+    ),
+  };
+  return (
+    <Dropdown
+      horizontalMenuAlign={'right'}
+      options={[{ options: controlPreviousOptions }]}
+      onChange={(opt) => {
+        setModalContents(
+          <span>
+            {controlPreviousDialogs[opt[0].value]}
+            <Button onClick={modalClose}>Close Dialog</Button>
+          </span>
+        );
+      }}
+    >
+      <div>Previous</div>
+    </Dropdown>
+  );
+};
+
+export interface NarrativeControlProps {
+  narrativeDoc: NarrativeDoc;
+}
+
+const NarrativeControl: FC<NarrativeControlProps> = ({ narrativeDoc }) => {
+  const { version } = narrativeDoc;
+  const { ver: verRaw } = useParams();
+  const ver = Math.min(Number(normalizeVersion(verRaw)), version);
+  return (
+    <>
+      {!ver || ver === version ? (
+        <ControlLatest narrativeDoc={narrativeDoc} />
+      ) : (
+        <ControlPrevious narrativeDoc={narrativeDoc} version={Number(ver)} />
+      )}
+    </>
+  );
+};
+
+export default NarrativeControl;
