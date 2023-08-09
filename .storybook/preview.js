b62945b7c3796b79bea16f57512081b04169fcc7
Dakota Blair <dblair@bnl.gov>
2023-08-09 16:13:57 -0400

    Add stories for narrative control components.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,26 @@
-import "../src/index.scss";
+import '../src/index.scss';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createTestStore } from '../src/app/store';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  options: {
+    storySort: (a, b) => {
+      const keyA = a[1].title;
+      const keyB = b[1].title;
+      return keyA === keyB
+        ? 0
+        : keyA.localeCompare(keyB, undefined, { numeric: true });
+    },
+  },
+};
 
 export const decorators = [
   (Story) => (
