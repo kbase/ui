475d3050d9c96097d4facebfc19265dd5b66dc67
David Lyon <dauglyon@users.noreply.github.com>
2023-10-06 14:44:46 -0700

    fix execution order

--- src/features/legacy/Fallback.tsx
+++ src/features/legacy/Fallback.tsx
@@ -1,30 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import PageNotFound from '../layout/PageNotFound';
 
 /**
  * 404s from the legacy site are redirected from legacy.DOMAIN/[some/path/here] to DOMAIN/fallback/[some/path/here]
+ * this component handles these fallback redirects for pages such as '/narratives'
  */
 export const Fallback = () => {
   const location = useParams();
   const navigate = useNavigate();
 
   const fallbackPath = location['*']?.toLowerCase();
+  const [notFound, setNotFound] = useState(false);
 
-  // Make sure we are in window.top (not within an iframe)
-  // do this in a useEffect to run it before the first render
   useEffect(() => {
-    if (window.top && window !== window.top) {
+    // Make sure we are in window.top (not within an iframe),
+    if (window && window.top && window !== window.top) {
+      // Not in top window, redirect top window to current location
       window.top.location = window.location;
+    } else {
+      // We are in the top window
+      // redirect appropriately based on the fallbackPath, otherwise set notFound to true
+      if (fallbackPath === 'narratives') {
+        navigate('/narratives');
+      } else {
+        setNotFound(true);
+      }
     }
-  }, []);
+  }, [fallbackPath, navigate]);
 
-  // redirect appropriately based on the fallbackPath, or render a PageNotFound
-  if (fallbackPath === 'narratives') {
-    navigate('/narratives');
-  } else {
+  // show PageNotFound if we don't have a fallback redirect
+  if (notFound) {
     return <PageNotFound />;
   }
-
   return <p>Redirecting...</p>;
 };
