ca938a3fd3d1028090a44c503418f3455b235be5
David Lyon <dauglyon@users.noreply.github.com>
2024-02-26 12:31:19 -0800

    use subdomain-acessible cookie domain

--- src/features/auth/hooks.ts
+++ src/features/auth/hooks.ts
@@ -64,7 +64,7 @@ export const useTokenCookie = (
         expires: new Date(expires),
         ...(process.env.NODE_ENV === 'development'
           ? {}
-          : { domain: process.env.REACT_APP_KBASE_DOMAIN }),
+          : { domain: `.${process.env.REACT_APP_KBASE_DOMAIN}` }),
       });
     } else if (token && !expires) {
       // eslint-disable-next-line no-console
