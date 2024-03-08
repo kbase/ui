038388dca7da1e96fbff78f8b14dd6536eb9deaf
Dakota Blair <dblair@bnl.gov>
2024-03-08 15:13:30 -0500

    RE2022-241: Added global.d.ts.

---
+++ src/types/global.d.ts
@@ -0,0 +1,7 @@
+/* globally declared types, automatically loaded by typescript. */
+interface Window {
+  gtag: Function; // google analytics
+  /* see <HeatMap /> component */
+  __kbase_resizeListenerRegistered: boolean | undefined;
+  __kbase_resizeHandler: () => void;
+}
