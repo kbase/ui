6a1e8ac90ecf0575b10dd206efc06d1478dd4af2
Dakota Blair <dblair@bnl.gov>
2024-03-13 14:53:43 -0400

    Fix tests.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,7 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import 'jest-canvas-mock';
 
 // Fixes test errors thrown by plotly.js dependency
 window.URL.createObjectURL = URL.createObjectURL ?? ((obj: Blob) => '');
