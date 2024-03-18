ea94a1757b63f274100198b4b17aa4b9c3cd478a
Erik Pearson <eapearson@lbl.gov>
2024-03-18 11:20:32 -0700

    add tests for default values [URO-188]

--- src/features/params/paramsSlice.test.tsx
+++ src/features/params/paramsSlice.test.tsx
@@ -24,7 +24,54 @@ describe('paramsSlice', () => {
     });
   });
 
-  test('setParams accepts search param "q"', async () => {
+  test('setParams accepts europa search params', async () => {
+    const store = createTestStore();
+    const params = {
+      limit: 'abc',
+      search: 'def',
+      sort: 'hij',
+      view: 'klm',
+      foo: 'bar',
+    };
+    const Component = () => {
+      store.dispatch(setParams(params));
+      return <></>;
+    };
+    render(
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    );
+    await waitFor(() => {
+      expect(store.getState().params.limit).toBe(params.limit);
+      expect(store.getState().params.search).toBe(params.search);
+      expect(store.getState().params.sort).toBe(params.sort);
+      expect(store.getState().params.view).toBe(params.view);
+      expect(store.getState().params.foo).toBeUndefined();
+    });
+  });
+
+  test('setParams accepts europa search params, using defaults', async () => {
+    const store = createTestStore();
+    const params = {};
+    const Component = () => {
+      store.dispatch(setParams(params));
+      return <></>;
+    };
+    render(
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    );
+    await waitFor(() => {
+      expect(store.getState().params.limit).toBe('20');
+      expect(store.getState().params.search).toBeNull();
+      expect(store.getState().params.sort).toBe('-updated');
+      expect(store.getState().params.view).toBe('data');
+    });
+  });
+
+  test('setParams accepts data search param "q"', async () => {
     const store = createTestStore();
     const params = { q: 'abc', foo: 'bar' };
     const Component = () => {
@@ -39,6 +86,23 @@ describe('paramsSlice', () => {
     await waitFor(() => {
       expect(store.getState().params.q).toBe(params.q);
       expect(store.getState().params.foo).toBeUndefined();
+    });
+  });
+
+  test('setParams sets data search params to default values', async () => {
+    const store = createTestStore();
+    const params = {};
+    const Component = () => {
+      store.dispatch(setParams(params));
+      return <></>;
+    };
+    render(
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    );
+    await waitFor(() => {
+      expect(store.getState().params.q).toBeNull();
     });
   });
 
@@ -57,6 +121,23 @@ describe('paramsSlice', () => {
     await waitFor(() => {
       expect(store.getState().params.tab).toBe(params.tab);
       expect(store.getState().params.foo).toBeUndefined();
+    });
+  });
+
+  test('setParams sets account management ui params to default values', async () => {
+    const store = createTestStore();
+    const params = {};
+    const Component = () => {
+      store.dispatch(setParams(params));
+      return <></>;
+    };
+    render(
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    );
+    await waitFor(() => {
+      expect(store.getState().params.tab).toBeNull();
     });
   });
 
@@ -79,6 +160,24 @@ describe('paramsSlice', () => {
     });
   });
 
+  test('setParams sets narrative opening params to their default values', async () => {
+    const store = createTestStore();
+    const params = {};
+    const Component = () => {
+      store.dispatch(setParams(params));
+      return <></>;
+    };
+    render(
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    );
+    await waitFor(() => {
+      expect(store.getState().params.n).toBeNull();
+      expect(store.getState().params.check).toBeNull();
+    });
+  });
+
   test('setParams accepts kbase-ui navigation params "nextrequest"', async () => {
     const store = createTestStore();
     const params = { nextrequest: 'some/where', source: 'baz', foo: 'bar' };
@@ -95,6 +194,24 @@ describe('paramsSlice', () => {
       expect(store.getState().params.nextrequest).toBe(params.nextrequest);
       expect(store.getState().params.source).toBe(params.source);
       expect(store.getState().params.foo).toBeUndefined();
+    });
+  });
+
+  test('setParams sets kbase-ui navigation params to default values', async () => {
+    const store = createTestStore();
+    const params = {};
+    const Component = () => {
+      store.dispatch(setParams(params));
+      return <></>;
+    };
+    render(
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    );
+    await waitFor(() => {
+      expect(store.getState().params.nextrequest).toBeNull();
+      expect(store.getState().params.source).toBeNull();
     });
   });
 
@@ -117,14 +234,32 @@ describe('paramsSlice', () => {
     });
   });
 
+  test('setParams sets kbase-ui landing page params to default values', async () => {
+    const store = createTestStore();
+    const params = {};
+    const Component = () => {
+      store.dispatch(setParams(params));
+      return <></>;
+    };
+    render(
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    );
+    await waitFor(() => {
+      expect(store.getState().params.sub).toBeNull();
+      expect(store.getState().params.subid).toBeNull();
+    });
+  });
+
   test('setParams accepts orcidlink params', async () => {
     const store = createTestStore();
     const params = {
       skip_prompt: 'abc',
       ui_options: 'def',
       return_link: 'hij',
-      code: 'jkl',
-      message: 'mno',
+      code: 'klm',
+      message: 'nop',
       foo: 'bar',
     };
     const Component = () => {
@@ -143,6 +278,27 @@ describe('paramsSlice', () => {
       expect(store.getState().params.code).toBe(params.code);
       expect(store.getState().params.message).toBe(params.message);
       expect(store.getState().params.foo).toBeUndefined();
+    });
+  });
+
+  test('setParams sets orcidlink params to default values', async () => {
+    const store = createTestStore();
+    const params = {};
+    const Component = () => {
+      store.dispatch(setParams(params));
+      return <></>;
+    };
+    render(
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    );
+    await waitFor(() => {
+      expect(store.getState().params.skip_prompt).toBeNull();
+      expect(store.getState().params.ui_options).toBeNull();
+      expect(store.getState().params.return_link).toBeNull();
+      expect(store.getState().params.code).toBeNull();
+      expect(store.getState().params.message).toBeNull();
     });
   });
 });
