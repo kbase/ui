ee4fe11ef3109071f6940df8b1f0adeb0f258ee0
David Lyon <dauglyon@users.noreply.github.com>
2024-03-26 11:28:45 -0700

    Make Collections NavItem public

--- src/features/layout/LeftNavBar.tsx
+++ src/features/layout/LeftNavBar.tsx
@@ -43,6 +43,13 @@ const LeftNavBar: FC = () => {
           icon={faBullhorn}
           badge={feeds?.unseen.global}
         />
+        <NavItem
+          path="/collections"
+          desc="Collections"
+          icon={faLayerGroup}
+          badge={'beta'}
+          badgeColor={'primary'}
+        />
         <DevNav />
       </ul>
     </nav>
@@ -51,6 +58,7 @@ const LeftNavBar: FC = () => {
 
 const devDomains = new Set(['', 'ci-europa.kbase.us']);
 
+/** Hidden navigation items for devs */
 const DevNav: FC = () => {
   const me = useAppSelector(authMe);
   useAuthMe();
@@ -62,13 +70,14 @@ const DevNav: FC = () => {
   if (!me || !dev) return <></>;
   return (
     <>
+      {/* Nothing here now, heres an example:
       <NavItem
         path="/collections"
         desc="Collections"
         icon={faLayerGroup}
         badge={'beta'}
         badgeColor={'primary'}
-      />
+      /> */}
     </>
   );
 };
