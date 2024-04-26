8b91ef8d9caae436f245da5c4dc0c8f30f14520a
David Lyon <dauglyon@users.noreply.github.com>
2024-04-25 17:05:22 -0700

    Make biolog context tabs function with loading and counts

--- src/features/collections/data_products/Biolog.tsx
+++ src/features/collections/data_products/Biolog.tsx
@@ -19,7 +19,10 @@ import { useAppDispatch } from '../../../common/hooks';
 import { formatNumber } from '../../../common/utils/stringUtils';
 import { useAppParam } from '../../params/hooks';
 import classes from '../Collections.module.scss';
-import { useGenerateSelectionId } from '../collectionsSlice';
+import {
+  useFilterContextState,
+  useGenerateSelectionId,
+} from '../collectionsSlice';
 import { useFilterContexts } from '../Filters';
 import { useProcessStatePolling } from '../hooks';
 import { HeatMap, HeatMapCallback, MAX_HEATMAP_PAGE } from './HeatMap';
@@ -105,25 +108,24 @@ const useBiolog = (collection_id: string | undefined) => {
   const selId = useGenerateSelectionId(collection_id || '', {
     skip: !collection_id,
   });
-  const [matchMark, setMatchMark] = useState<boolean>(true);
-  const [selMark, setSelMark] = useState<boolean>(true);
   const [pagination, setPagination] = useState<PaginationState>({
     pageIndex: 0,
     pageSize: 56,
   });
+  const context = useFilterContextState(collection_id);
 
   const pageLastIdCache: Record<string, string> = useMemo(
     () => ({}),
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [collection_id, selMark, matchMark, pagination.pageSize]
+    [collection_id, context, pagination.pageSize]
   );
 
   const heatMapParams = useMemo(
     () => ({
       collection_id: collection_id ?? '',
       limit: pagination.pageSize,
-      match_mark: false,
-      selection_mark: false,
+      match_mark: !context.endsWith('.matched'),
+      selection_mark: !context.endsWith('.selected'),
       ...(pagination.pageIndex !== 0
         ? { start_after: pageLastIdCache[pagination.pageIndex - 1] }
         : {}),
@@ -137,6 +139,7 @@ const useBiolog = (collection_id: string | undefined) => {
       pagination.pageIndex,
       pagination.pageSize,
       selId,
+      context,
     ]
   );
   const allCountParams = useMemo(
@@ -184,17 +187,37 @@ const useBiolog = (collection_id: string | undefined) => {
     skip: !collection_id,
   });
 
-  const matchCount = getBiolog.useQuery(
-    { ...allCountParams, match_mark: false },
-    {
-      skip: !collection_id,
-    }
+  const [matchCountParams, selCountParams] = useMemo(
+    () => [
+      {
+        ...allCountParams,
+        match_mark: false,
+      },
+      {
+        ...allCountParams,
+        selection_mark: false,
+      },
+    ],
+    [allCountParams]
   );
 
-  const selCount = getBiolog.useQuery(
-    { ...allCountParams, selection_mark: false },
+  const matchCount = getBiolog.useQuery(matchCountParams, {
+    skip: !collection_id || !matchId,
+  });
+
+  const { match_state } = useProcessStatePolling(matchCount, ['match_state'], {
+    skipPoll: !collection_id || !matchId,
+  });
+
+  const selCount = getBiolog.useQuery(selCountParams, {
+    skip: !collection_id || !selId,
+  });
+
+  const { selection_state } = useProcessStatePolling(
+    selCount,
+    ['selection_state'],
     {
-      skip: !collection_id,
+      skipPoll: !collection_id || !selId,
     }
   );
 
@@ -209,12 +232,18 @@ const useBiolog = (collection_id: string | undefined) => {
       value: 'biolog.matched',
       count: heatMapParams.match_id ? matchCount?.data?.count : undefined,
       disabled: !heatMapParams.match_id || matchCount?.data?.count === 0,
+      loading:
+        (heatMapParams.match_id && !match_state) ||
+        match_state === 'processing',
     },
     {
       label: 'Selected',
       value: 'biolog.selected',
       count: heatMapParams.selection_id ? selCount?.data?.count : undefined,
       disabled: !heatMapParams.selection_id || selCount?.data?.count === 0,
+      loading:
+        (heatMapParams.selection_id && !selection_state) ||
+        selection_state === 'processing',
     },
   ]);
 
@@ -297,8 +326,6 @@ const useBiolog = (collection_id: string | undefined) => {
   });
 
   return {
-    setMatchMark,
-    setSelMark,
     setPagination,
     biolog,
     biologQuery,
