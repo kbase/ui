bd504a6b1e106eb31041883aca2857959df40d56
Erik Pearson <eapearson@lbl.gov>
2024-05-17 10:16:18 -0700

    remove comments [URO-208]

--- src/features/orcidlink/HomeUnlinked.tsx
+++ src/features/orcidlink/HomeUnlinked.tsx
@@ -19,15 +19,6 @@ export default function Unlinked() {
       <Grid xs={6}>
         <Card variant="outlined">
           <CardHeader title="Create Your KBase ORCID® Link!" />
-          {/* 
-            A note on the padding and margin overrides. Mui is not very smart about components
-            embedded in card content. The card content has a padding on all subelements (header, 
-            content, actions) which is added to any margins and padding of components contained 
-            within, so it pretty easily blows up into a lot of blank space. 
-            To keep the empty space under control (and others have commented on this 
-            in GH issues), one must fiddle with padding and margins on a case-by-case
-            basis.
-          ) */}
           <CardContent>
             <Typography>
               You do not currently have a link from your KBase account to an
@@ -37,9 +28,6 @@ export default function Unlinked() {
               Click the button below to begin the KBase ORCID® Link process.
             </Typography>
           </CardContent>
-          {/* Note that the card actions padding is overridden so that it matches 
-              that of the card content and header. There are a number of formatting 
-              issues with Cards. Some will apparently be fixed in v6. */}
           <CardActions style={{ justifyContent: 'center' }} sx={{ p: 2 }}>
             <Button
               variant="contained"
@@ -58,7 +46,6 @@ export default function Unlinked() {
             <Typography>NOTES HERE</Typography>
           </CardContent>
         </Card>
-
         <Card variant="outlined" sx={{ mt: 2 }}>
           <CardHeader title="More Information" />
           <CardContent>
