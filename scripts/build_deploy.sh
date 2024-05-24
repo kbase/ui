1966afb356e62d089d68018ef9e16c6ff05363ab
Dakota Blair <dblair@bnl.gov>
2024-05-24 14:03:26 -0400

    Add build artifact.
    It may be time to convert the build_deploy.sh script to python.

--- scripts/build_deploy.sh
+++ scripts/build_deploy.sh
@@ -10,7 +10,8 @@ for enviro in "${enviromentsConfig[@]}"; do
   read -a envConf <<< "$enviro"
   echo "Building static files for enviroment \"${envConf[0]}\"...";
 
-  BUILD_PATH="./deploy/${envConf[0]}" \
+  BUILD_PATH="./deploy/${envConf[0]}";
+  BUILD_PATH="$BUILD_PATH" \
   REACT_APP_KBASE_ENV="${envConf[0]}" \
   REACT_APP_KBASE_DOMAIN="${envConf[1]}" \
   REACT_APP_KBASE_LEGACY_DOMAIN="${envConf[2]}" \
@@ -18,5 +19,13 @@ for enviro in "${enviromentsConfig[@]}"; do
   REACT_APP_KBASE_BACKUP_COOKIE_NAME="${envConf[4]}" \
   REACT_APP_KBASE_BACKUP_COOKIE_DOMAIN="${envConf[5]}" \
   npm run build && \
+  echo "{
+    'environment': '${envConf[0]}',
+    'hash': '$(git rev-parse HEAD)',
+    'name': 'Europa',
+    'tag': '$(git describe --tags)'
+  }" | jq -R -s '.
+    | gsub("[&-(]"; "\"")
+    | fromjson' > "$BUILD_PATH/build.json" && \
   echo "Built static files for enviroment \"${envConf[0]}\".";
 done
