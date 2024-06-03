b02d1c3fce1a51b5550027f7f6f25640b293f0b5
Dakota Blair <dblair@bnl.gov>
2024-06-03 17:00:48 -0400

    WIP002

---
+++ scripts/build_deploy.ts
@@ -0,0 +1,56 @@
+#!/usr/bin/env ts-node
+
+// invoke using
+// node --loader ts-node/esm ./scripts/build_deploy.ts
+
+// This node script uses CommonJS require
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+// import { spawn } from 'node:child_process';
+
+// import * as config from '../config.json';
+const { spawn } = require('node:child_process');
+
+const config = require('../config.json');
+
+/*
+console.log({ config }); // eslint-disable-line no-console
+
+const environment = '';
+const data = '';
+*/
+
+const build = (environment, { domain, legacy, public_url, backup_cookie }) => {
+  let count = 0;
+  const envsNew = {
+    BUILD_PATH: `./deploy/${environment}`,
+    PUBLIC_URL: public_url,
+    REACT_APP_KBASE_ENV: environment,
+    REACT_APP_KBASE_DOMAIN: domain,
+    REACT_APP_KBASE_LEGACY_DOMAIN: legacy,
+    REACT_APP_KBASE_BACKUP_COOKIE_NAME: backup_cookie?.name,
+  };
+
+  Object.assign(process.env, envsNew);
+  const proc = spawn('npm', ['run', 'build'], { timeout: 60000 });
+  proc.stdout.on('data', (data) => {
+    // eslint-disable-next-line no-console
+    console.log({ count, stdout: `${data}` });
+    count += 1;
+  });
+};
+
+const main = () => {
+  const envs = config.environments;
+  Object.keys(envs).forEach((env) => {
+    console.log({ env, data: envs[env] }); // eslint-disable-line no-console
+    build(env, envs[env]);
+  });
+  build('nodetest', config.environments.ci);
+};
+
+if (require.main === module) {
+  main();
+}
+
+module.exports = {};
