fae3ffee00515062e06c1b3b625cddf944af06f4
Dakota Blair <dblair@bnl.gov>
2024-06-04 16:37:55 -0400

    WIP003

--- scripts/build_deploy.ts
+++ scripts/build_deploy.ts
@@ -13,6 +13,18 @@ const { spawn } = require('node:child_process');
 
 const config = require('../config.json');
 
+interface Envs {
+  [key: string]: string;
+}
+
+interface EnvConfig {
+  domain: string;
+  legacy: string;
+  public_url: string;
+  backup_cookie?: {
+    name: string;
+  };
+}
 /*
 console.log({ config }); // eslint-disable-line no-console
 
@@ -20,20 +32,24 @@ const environment = '';
 const data = '';
 */
 
-const build = (environment, { domain, legacy, public_url, backup_cookie }) => {
+const build = (
+  environment: string,
+  { domain, legacy, public_url, backup_cookie }: EnvConfig
+) => {
   let count = 0;
-  const envsNew = {
+  const envsNew: Envs = {
     BUILD_PATH: `./deploy/${environment}`,
     PUBLIC_URL: public_url,
     REACT_APP_KBASE_ENV: environment,
     REACT_APP_KBASE_DOMAIN: domain,
     REACT_APP_KBASE_LEGACY_DOMAIN: legacy,
-    REACT_APP_KBASE_BACKUP_COOKIE_NAME: backup_cookie?.name,
+    REACT_APP_KBASE_BACKUP_COOKIE_NAME: backup_cookie?.name || '',
   };
 
   Object.assign(process.env, envsNew);
-  const proc = spawn('npm', ['run', 'build'], { timeout: 60000 });
-  proc.stdout.on('data', (data) => {
+  // const proc = spawn('npm', ['run', 'build'], { timeout: 60000 });
+  const proc = spawn('echo', ['npm', 'run', 'build'], { timeout: 60000 });
+  proc.stdout.on('data', (data: string) => {
     // eslint-disable-next-line no-console
     console.log({ count, stdout: `${data}` });
     count += 1;
@@ -43,7 +59,7 @@ const build = (environment, { domain, legacy, public_url, backup_cookie }) => {
 const main = () => {
   const envs = config.environments;
   Object.keys(envs).forEach((env) => {
-    console.log({ env, data: envs[env] }); // eslint-disable-line no-console
+    // console.log({ env, data: envs[env] }); // eslint-disable-line no-console
     build(env, envs[env]);
   });
   build('nodetest', config.environments.ci);
@@ -53,4 +69,4 @@ if (require.main === module) {
   main();
 }
 
-module.exports = {};
+//export {};
