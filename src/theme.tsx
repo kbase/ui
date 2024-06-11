44763c11ad1b4667b02591c84848eced047b09a7
Cody O'Donnell <ctodonnell@lbl.gov>
2024-06-11 15:18:06 -0700

    Remove test text from comment

--- src/theme.tsx
+++ src/theme.tsx
@@ -13,7 +13,7 @@ const RouterCompatibleLink = forwardRef<
   return <RouterLink ref={ref} to={href} {...other} />;
 });
 
-// TODO: import from single source of truth (test)
+// TODO: import from single source of truth
 const baseColor = 'rgb(62, 56, 50)';
 
 export const theme = createTheme({
