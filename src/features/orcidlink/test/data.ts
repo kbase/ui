eecd27093cd2d18a0392652966450bc6d27ebac2
Erik Pearson <eapearson@lbl.gov>
2024-06-25 16:42:53 +0000

    add more test data [URO-211]

--- src/features/orcidlink/test/data.ts
+++ src/features/orcidlink/test/data.ts
@@ -1,16 +1,52 @@
+/**
+ * Contains test data used in orcidlink tests.
+ *
+ * Most test data should reside here.
+ */
+
 import { InfoResult } from '../../../common/api/orcidlinkAPI';
 import {
   LinkRecordPublic,
+  LinkRecordPublicNonOwner,
   ORCIDProfile,
 } from '../../../common/api/orcidLinkCommon';
 import { JSONRPC20Error } from '../../../common/api/utils/kbaseBaseQuery';
-import orcidlinkIsLinkedAuthorizationRequired from './data/orcidlink-is-linked-1010.json';
+import {
+  ErrorInfoResult,
+  LinkingSessionPublicComplete,
+  StatusResult,
+} from '../common/api/ORCIDLInkAPI';
 
-export const ORCIDLINK_IS_LINKED_AUTHORIZATION_REQUIRED: JSONRPC20Error =
-  orcidlinkIsLinkedAuthorizationRequired;
+// We can have a short default timeout, as tests should be running against a
+// local server with very low latency.
+//
+// Of course if you are testing timeout errors, you should ignore this and use
+// whatever values are required to trigger whatever conditions are needed.
+export const API_CALL_TIMEOUT = 1000;
+
+export const STATUS_1: StatusResult = {
+  status: 'ok',
+  current_time: 123,
+  start_time: 456,
+};
+
+export const ERROR_INFO_1: ErrorInfoResult = {
+  error_info: {
+    code: 123,
+    title: 'Foo Error',
+    description: 'This is the foo error',
+    status_code: 400,
+  },
+};
+
+export const ORCIDLINK_IS_LINKED_AUTHORIZATION_REQUIRED: JSONRPC20Error = {
+  code: 1010,
+  message: 'Authorization Required',
+  data: 'Authorization Required',
+};
 
 export const PROFILE_1: ORCIDProfile = {
-  orcidId: '0009-0006-1955-0944',
+  orcidId: 'foo_orcid_id',
   nameGroup: {
     private: false,
     fields: {
@@ -39,6 +75,22 @@ export const PROFILE_1: ORCIDProfile = {
   ],
 };
 
+export const LINKING_SESSION_1: LinkingSessionPublicComplete = {
+  session_id: 'f85a9b43-5dd4-4e1d-ad66-9f923fde5de2',
+  username: 'kbaseuitest',
+  created_at: 1718044771667,
+  expires_at: 1718045371667,
+  return_link: null,
+  skip_prompt: false,
+  ui_options: '',
+  orcid_auth: {
+    name: 'Erik T. Pearson',
+    scope: '/read-limited /activities/update',
+    expires_in: 631138518,
+    orcid: '0009-0008-7728-946X',
+  },
+};
+
 export const LINK_RECORD_1: LinkRecordPublic = {
   username: 'foo',
   created_at: 1714546800000,
@@ -49,6 +101,14 @@ export const LINK_RECORD_1: LinkRecordPublic = {
     name: 'foo bar',
     orcid: 'abc123',
     scope: '/read-limited',
+  },
+};
+
+export const LINK_RECORD_OTHER_1: LinkRecordPublicNonOwner = {
+  username: 'bar',
+  orcid_auth: {
+    name: 'Bar',
+    orcid: 'bar_orcid_id',
   },
 };
 
@@ -90,6 +150,32 @@ export const INITIAL_STORE_STATE = {
       name: 'Foo Bar',
       type: 'Login',
       user: 'foo',
+      cachefor: 890,
+    },
+    initialized: true,
+  },
+};
+
+export const INITIAL_STORE_STATE_UNAUTHENTICATED = {
+  auth: {
+    token: undefined,
+    username: undefined,
+    tokenInfo: undefined,
+    initialized: false,
+  },
+};
+
+export const INITIAL_STORE_STATE_BAR = {
+  auth: {
+    token: 'bar_token',
+    username: 'bar',
+    tokenInfo: {
+      created: 123,
+      expires: 456,
+      id: 'abc123',
+      name: 'Bar Baz',
+      type: 'Login',
+      user: 'bar',
       cachefor: 890,
     },
     initialized: true,
