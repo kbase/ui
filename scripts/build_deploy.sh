6831ed8807114a2711eb14c9d7593bece4caa3d8
Dakota Blair <dblair@bnl.gov>
2024-07-01 16:56:45 -0400

    WIP009

--- scripts/build_deploy.sh
+++ scripts/build_deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+DATE=$(date -Iseconds) \
 HASH=$(git rev-parse HEAD) \
 TAG=$(git describe --tags) \
 node_modules/.bin/ts-node scripts/build_deploy.ts
--- scripts/build_deploy.ts
+++ scripts/build_deploy.ts
@@ -13,6 +13,7 @@ interface Envs {
   [key: string]: string;
 }
 
+/* This is the schema for an environment in config.json */
 interface EnvConfig {
   domain: string;
   legacy: string;
@@ -22,8 +23,10 @@ interface EnvConfig {
   };
 }
 
+/* Run npm run build for a given environment. */
 const build = async (
   environment: string,
+  date: string,
   hash: string,
   tag: string,
   envConfig: EnvConfig
@@ -41,8 +44,9 @@ const build = async (
     publicURL,
     backupCookie,
   });
+
+  /* Update environment variables to be used by npm run build */
   const buildPath = `./deploy/${environment}`;
-  const buildDate = new Date().toISOString();
   const envsNew: Envs = {
     BUILD_PATH: buildPath,
     PUBLIC_URL: publicURL,
@@ -51,18 +55,26 @@ const build = async (
     REACT_APP_KBASE_LEGACY_DOMAIN: legacy,
     REACT_APP_KBASE_BACKUP_COOKIE_NAME: backupCookie?.name || '',
   };
-
   Object.assign(process.env, envsNew);
+
+  /* Return a promise that resolves when npm run build is finished and the
+   * build artifact is written.
+   */
   return new Promise((resolve) => {
     const proc = spawn('npm', ['run', 'build'], { timeout: 60000 });
+
+    /* Print stdout. This is not necessary, strictly speaking, but is a good
+     * place to start if something goes wrong.
+     */
     proc.stdout.on('data', (data: Buffer) => {
       console.log(data.toString()); // eslint-disable-line no-console
     });
 
+    /* On close, write the build artifact and resolve the promise. */
     proc.on('close', async (code: number) => {
       const buildArtifact = {
-        date: buildDate,
         name: 'Europa',
+        date,
         environment,
         hash,
         tag,
@@ -77,29 +89,39 @@ const build = async (
       } finally {
         await filehandle?.close();
       }
-      console.log(`Build for ${environment} complete.`); // eslint-disable-line no-console
+      // eslint-disable-next-line no-console
+      console.log(`Build for ${environment} complete.`);
       resolve(buildArtifact);
     });
   });
 };
 
+/* Take an array and split it into chunks of size at most chunkSize. */
+const chunk = <T>(items: T[], chunkSize: number) => {
+  const nChunks = Math.floor(items.length / chunkSize) + 1;
+  const chunks: T[][] = Array(nChunks)
+    .fill(0)
+    .map(() => []);
+  items.forEach((item, ix) => {
+    const index = Math.floor(ix / chunkSize);
+    chunks[Math.floor(index)].push(item);
+  });
+  return chunks;
+};
+
+/* Make it so. */
 const main = async () => {
   const envs = config.environments;
+  const date = process.env.DATE || new Date().toISOString();
   const hash = process.env.HASH || '';
   const tag = process.env.TAG || '';
   const keysSort = Object.keys(envs).sort();
-  const groupSize = 4;
-  const nGroups = Math.floor(keysSort.length / groupSize) + 1;
-  const envGroups: string[][] = Array(nGroups)
-    .fill(0)
-    .map(() => []);
-  keysSort.forEach((env, ix) => {
-    const index = Math.floor(ix / groupSize);
-    envGroups[Math.floor(index)].push(env);
-  });
+  const maxWorkers = 4;
+  const envGroups = chunk(keysSort, maxWorkers);
   for (const group in envGroups) {
+    /* Run at most maxWorkers builds at a time: */
     await Promise.all(
-      envGroups[group].map((env) => build(env, hash, tag, envs[env]))
+      envGroups[group].map((env) => build(env, date, hash, tag, envs[env]))
     );
   }
 };
