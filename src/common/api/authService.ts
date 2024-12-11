5303897f3e8b89076a6bc28b538d055145a0a09d
David Lyon <dauglyon@users.noreply.github.com>
2024-12-10 16:17:54 -0800

    Add AccountInfo state/queries

--- src/common/api/authService.ts
+++ src/common/api/authService.ts
@@ -22,6 +22,10 @@ interface AuthParams {
   getMe: {
     token: string;
   };
+  setMe: {
+    token: string;
+    meUpdate: Pick<Me, 'display' | 'email'>;
+  };
   getUsers: {
     token: string;
     users: string[];
@@ -45,6 +49,7 @@ interface AuthParams {
 
 interface AuthResults {
   getMe: Me;
+  setMe: void;
   getUsers: Record<string, string>;
   searchUsers: Record<string, string>;
   getLoginChoice: {
@@ -100,21 +105,35 @@ interface AuthResults {
 }
 
 // Auth does not use JSONRpc, so we use queryFn to make custom queries
-export const authApi = baseApi.injectEndpoints({
-  endpoints: (builder) => ({
-    authFromToken: builder.query<TokenResponse, string>({
-      query: (token) =>
-        authService({
-          url: '/api/V2/token',
-          method: 'GET',
-          headers: {
-            Authorization: token || '',
-          },
-        }),
-    }),
-    getMe: builder.query<AuthResults['getMe'], AuthParams['getMe']>({
-      query: ({ token }) => {
-        /* I want to do
+export const authApi = baseApi
+  .enhanceEndpoints({ addTagTypes: ['AccountMe'] })
+  .injectEndpoints({
+    endpoints: (builder) => ({
+      authFromToken: builder.query<TokenResponse, string>({
+        query: (token) =>
+          authService({
+            url: '/api/V2/token',
+            method: 'GET',
+            headers: {
+              Authorization: token || '',
+            },
+          }),
+      }),
+      getMe: builder.query<AuthResults['getMe'], AuthParams['getMe']>({
+        query: ({ token }) => {
+          return authService({
+            headers: {
+              Authorization: token,
+            },
+            method: 'GET',
+            url: '/api/V2/me',
+          });
+        },
+        providesTags: ['AccountMe'],
+      }),
+      setMe: builder.mutation<AuthResults['setMe'], AuthParams['setMe']>({
+        query: ({ token, meUpdate }) => {
+          /* I want to do
         const token = store.getState().auth.token;
         but authSlice imports revokeToken defined here,
         so this becomes a circular depenency.
@@ -123,106 +142,109 @@ export const authApi = baseApi.injectEndpoints({
           type annotation and is referenced directly or indirectly in its own
           initializer.
         */
-        return authService({
-          headers: {
-            Authorization: token,
-          },
-          method: 'GET',
-          url: '/api/V2/me',
-        });
-      },
+          return authService({
+            headers: {
+              Authorization: token,
+            },
+            method: 'PUT',
+            url: '/me',
+            body: meUpdate,
+          });
+        },
+        invalidatesTags: ['AccountMe'],
+      }),
+      getUsers: builder.query<AuthResults['getUsers'], AuthParams['getUsers']>({
+        query: ({ token, users }) =>
+          authService({
+            headers: {
+              Authorization: token,
+            },
+            method: 'GET',
+            params: { list: users.join(',') },
+            url: '/api/V2/users',
+          }),
+      }),
+      searchUsers: builder.query<
+        AuthResults['searchUsers'],
+        AuthParams['searchUsers']
+      >({
+        query: ({ search, token }) =>
+          authService({
+            headers: {
+              Authorization: token,
+            },
+            method: 'GET',
+            url: `/api/V2/users/search/${search}`,
+          }),
+      }),
+      revokeToken: builder.mutation<boolean, string>({
+        query: (tokenId) =>
+          authService({
+            url: encode`/tokens/revoke/${tokenId}`,
+            method: 'DELETE',
+          }),
+      }),
+      getLoginChoice: builder.query<
+        AuthResults['getLoginChoice'],
+        AuthParams['getLoginChoice']
+      >({
+        query: () =>
+          // MUST have an in-process-login-token cookie
+          authService({
+            headers: {
+              accept: 'application/json',
+            },
+            method: 'GET',
+            url: '/login/choice',
+          }),
+      }),
+      postLoginPick: builder.mutation<
+        AuthResults['postLoginPick'],
+        AuthParams['postLoginPick']
+      >({
+        query: (pickedChoice) =>
+          authService({
+            url: encode`/login/pick`,
+            body: pickedChoice,
+            method: 'POST',
+          }),
+      }),
+      loginUsernameSuggest: builder.query<
+        AuthResults['loginUsernameSuggest'],
+        AuthParams['loginUsernameSuggest']
+      >({
+        query: (username) =>
+          // MUST have an in-process-login-token cookie
+          authService({
+            headers: {
+              accept: 'application/json',
+            },
+            method: 'GET',
+            url: `/login/suggestname/${encodeURIComponent(username)}`,
+          }),
+      }),
+      loginCreate: builder.mutation<
+        AuthResults['loginCreate'],
+        AuthParams['loginCreate']
+      >({
+        query: (params) =>
+          // MUST have an in-process-login-token cookie
+          authService({
+            headers: {
+              accept: 'application/json',
+            },
+            method: 'POST',
+            body: params,
+            url: `/login/create/`,
+          }),
+      }),
     }),
-    getUsers: builder.query<AuthResults['getUsers'], AuthParams['getUsers']>({
-      query: ({ token, users }) =>
-        authService({
-          headers: {
-            Authorization: token,
-          },
-          method: 'GET',
-          params: { list: users.join(',') },
-          url: '/api/V2/users',
-        }),
-    }),
-    searchUsers: builder.query<
-      AuthResults['searchUsers'],
-      AuthParams['searchUsers']
-    >({
-      query: ({ search, token }) =>
-        authService({
-          headers: {
-            Authorization: token,
-          },
-          method: 'GET',
-          url: `/api/V2/users/search/${search}`,
-        }),
-    }),
-    revokeToken: builder.mutation<boolean, string>({
-      query: (tokenId) =>
-        authService({
-          url: encode`/tokens/revoke/${tokenId}`,
-          method: 'DELETE',
-        }),
-    }),
-    getLoginChoice: builder.query<
-      AuthResults['getLoginChoice'],
-      AuthParams['getLoginChoice']
-    >({
-      query: () =>
-        // MUST have an in-process-login-token cookie
-        authService({
-          headers: {
-            accept: 'application/json',
-          },
-          method: 'GET',
-          url: '/login/choice',
-        }),
-    }),
-    postLoginPick: builder.mutation<
-      AuthResults['postLoginPick'],
-      AuthParams['postLoginPick']
-    >({
-      query: (pickedChoice) =>
-        authService({
-          url: encode`/login/pick`,
-          body: pickedChoice,
-          method: 'POST',
-        }),
-    }),
-    loginUsernameSuggest: builder.query<
-      AuthResults['loginUsernameSuggest'],
-      AuthParams['loginUsernameSuggest']
-    >({
-      query: (username) =>
-        // MUST have an in-process-login-token cookie
-        authService({
-          headers: {
-            accept: 'application/json',
-          },
-          method: 'GET',
-          url: `/login/suggestname/${encodeURIComponent(username)}`,
-        }),
-    }),
-    loginCreate: builder.mutation<
-      AuthResults['loginCreate'],
-      AuthParams['loginCreate']
-    >({
-      query: (params) =>
-        // MUST have an in-process-login-token cookie
-        authService({
-          headers: {
-            accept: 'application/json',
-          },
-          method: 'POST',
-          body: params,
-          url: `/login/create/`,
-        }),
-    }),
-  }),
-});
+  });
 
 export const {
   authFromToken,
   getMe,
+  setMe,
   getUsers,
   searchUsers,
   revokeToken,
--- src/features/account/AccountInfo.tsx
+++ src/features/account/AccountInfo.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faInfoCircle, faX } from '@fortawesome/free-solid-svg-icons';
 import {
   Button,
   FormControl,
@@ -10,11 +10,78 @@ import {
   Typography,
 } from '@mui/material';
 import { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import { Loader } from '../../common/components';
+import { emailRegex } from '../../common';
+import { getMe, setMe } from '../../common/api/authService';
+import { useAppSelector } from '../../common/hooks';
+import {
+  getUserProfile,
+  setUserProfile,
+} from '../../common/api/userProfileApi';
 
 /**
  * Content for the Account tab in the Account page
  */
 export const AccountInfo: FC = () => {
+  const token = useAppSelector((s) => s.auth.token) ?? '';
+  const username = useAppSelector((s) => s.auth.username) ?? '';
+
+  // Profile
+  const profiles = getUserProfile.useQuery({ usernames: [username] });
+  const profile = profiles.data?.[0]?.[0];
+  const [triggerSetProfile, setProfileResult] = setUserProfile.useMutation();
+  // Account
+  const account = getMe.useQuery({ token });
+  const [triggerSetMe, setMeResult] = setMe.useMutation();
+
+  const form = useForm<{ name: string; email: string }>({
+    values: {
+      name: account.data?.display ?? '',
+      email: account.data?.email ?? '',
+    },
+    mode: 'onChange',
+  });
+
+  const onSubmit = form.handleSubmit((formData) => {
+    if (!profile) throw new Error('Error, undefined profile cannot be set');
+    triggerSetProfile([
+      {
+        profile: {
+          user: {
+            username: profile.user.username,
+            realname: formData.name,
+          },
+          profile: profile.profile,
+        },
+      },
+      token,
+    ]);
+    triggerSetMe({
+      token,
+      meUpdate: {
+        display: formData.name,
+        email: formData.email,
+      },
+    });
+  });
+
+  const onReset = () => {
+    setMeResult.reset();
+    setProfileResult.reset();
+    form.reset();
+    profiles.refetch();
+    account.refetch();
+  };
+
+  const loading = setProfileResult.isLoading || setMeResult.isLoading;
+  const complete =
+    !setProfileResult.isUninitialized &&
+    !setMeResult.isUninitialized &&
+    setProfileResult.isSuccess &&
+    setMeResult.isSuccess;
+  const error = setProfileResult.isError || setMeResult.isError;
+
   return (
     <Stack
       spacing={4}
@@ -37,35 +104,79 @@ export const AccountInfo: FC = () => {
           </Button>
         </Tooltip>
       </Stack>
-      <Stack spacing={2}>
-        <FormControl>
-          <FormLabel htmlFor="username-input">Name</FormLabel>
-          <TextField
-            id="username-input"
-            helperText="Your real name, displayed to other KBase users"
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="email-input">Email</FormLabel>
-          <TextField
-            id="email-input"
-            helperText="KBase may use this email address to communicate important information about KBase or your account. KBase will not share your email address with anyone, and other KBase users will not be able to see it."
-          />
-        </FormControl>
-      </Stack>
+      <form onSubmit={onSubmit}>
+        <Stack spacing={2}>
+          <FormControl>
+            <FormLabel htmlFor="username-input">Name</FormLabel>
+            <TextField
+              id="username-input"
+              {...form.register('name', {
+                required: true,
+                minLength: 1,
+              })}
+              helperText="Your real name, displayed to other KBase users"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="email-input">Email</FormLabel>
+            <TextField
+              id="email-input"
+              color={form.formState.errors.email ? 'error' : undefined}
+              {...form.register('email', {
+                required: true,
+                pattern: {
+                  value: emailRegex,
+                  message: 'Invalid email address',
+                },
+              })}
+              helperText="KBase may use this email address to communicate important information about KBase or your account. KBase will not share your email address with anyone, and other KBase users will not be able to see it."
+            />
+          </FormControl>
+          <Stack spacing={1} direction="row">
+            <Button
+              variant="outlined"
+              color="base"
+              size="large"
+              onClick={onReset}
+            >
+              Reset
+            </Button>
+            <Button
+              type="submit"
+              variant={!form.formState.isValid ? 'outlined' : 'contained'}
+              endIcon={
+                <Loader loading={loading} type="spinner">
+                  {complete ? (
+                    <FontAwesomeIcon icon={faCheck} />
+                  ) : error ? (
+                    <FontAwesomeIcon icon={faX} />
+                  ) : undefined}
+                </Loader>
+              }
+              size="large"
+            >
+              {'Save'}
+            </Button>
+          </Stack>
+        </Stack>
+      </form>
       <Typography variant="h2">Account Info</Typography>
       <Stack spacing={2}>
         <Stack>
           <Typography fontWeight="bold">Username</Typography>
-          <Typography>coolkbasehuman</Typography>
+          <Typography>{account.data?.user}</Typography>
         </Stack>
         <Stack>
           <Typography fontWeight="bold">Account Created</Typography>
-          <Typography>Apr 19, 2023 at 9:32am</Typography>
+          <Typography>
+            {new Date(account.data?.created ?? 0).toLocaleString()}
+          </Typography>
         </Stack>
         <Stack>
           <Typography fontWeight="bold">Last Sign In</Typography>
-          <Typography>3 days ago (Jul 9, 2024 at 9:05am)</Typography>
+          <Typography>
+            {new Date(account.data?.lastlogin ?? 0).toLocaleString()}
+          </Typography>
         </Stack>
       </Stack>
     </Stack>
