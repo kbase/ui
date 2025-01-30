4055a3abff08b62a7dbb2ede8023233b8c35dcdc
David Lyon <dauglyon@users.noreply.github.com>
2025-01-30 13:09:17 -0800

    Remove old OrcidLink feature, create orcidlink service API/types

--- src/features/orcidlink/common/PrivateField.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import PrivateField from './PrivateField';
-
-describe('The PrivateField component', () => {
-  it('renders the expected text', () => {
-    const { container } = render(<PrivateField />);
-    expect(container).toHaveTextContent('private');
-  });
-});
