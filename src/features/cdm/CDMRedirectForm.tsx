872463f1263cb918dd241da2c4d8c5e44f6e454f
David Lyon <dauglyon@users.noreply.github.com>
2025-02-05 14:58:43 -0800

    Update CDM login redirect URL

--- src/features/cdm/CDMRedirectForm.tsx
+++ src/features/cdm/CDMRedirectForm.tsx
@@ -5,9 +5,8 @@ export const CDMRedirectForm = ({
 }) => {
   // All this page does is send a POST login request to the CDM
   // User will need a cookie for this to succeed.
-  //TODO: UDPATE THIS URL
-  const CDMOrigin = 'http://127.0.0.1:4043';
-  const loginUrl = `${CDMOrigin}/hub/login`;
+  const KBASE_DOMAIN = process.env.REACT_APP_KBASE_DOMAIN || 'ci.kbase.us';
+  const loginUrl = `https://cdmhub.${KBASE_DOMAIN}/hub/login`;
   return (
     <form
       style={{ display: 'none' }}
