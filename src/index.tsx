44dec9af259db99ae5ae00ea3e80590ce5a66357
David Lyon <5115845+dauglyon@users.noreply.github.com>
2026-01-15 20:42:02 -0800

    Remove unused web-vitals CRA boilerplate
    web-vitals was included by Create React App but never configured
    to send metrics anywhere. Removing dead code.

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 import App from './app/App';
 import { store } from './app/store';
-import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@mui/material';
 import { theme } from './theme';
 
@@ -26,10 +25,5 @@ const Root: React.FC = () => (
 );
 
 root && root.render(<Root />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 
 export default Root;
